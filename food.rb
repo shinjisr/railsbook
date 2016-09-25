7deac2bc999ed204e346fedca15997fcc8deaca6
shinjisr <shinjisrg@gmail.com>
2016-09-25 17:51:10 +0800

    add more options in food.rb

--- food.rb
+++ food.rb
@@ -1,8 +1,11 @@
 loop do 
 	puts "Feed me some food!"
 	food = gets.strip
-	if food == "Pizza"
+    break if food.empty?
+	if food == "pizza"
 		puts "Yum, I love pizza!" 
+	elsif food == "shit"
+		puts "you eat your shit!"
 	else
 		puts "I don't want this!"
 end
