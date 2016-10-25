dc3842e0befd7c52aed1c99e9fe8e1d08139e386
shinjisr <shinjisrg@gmail.com>
2016-10-25 18:28:31 +0800

    add yell.rb, copy some functions to functions.rb

--- functions.rb
+++ functions.rb
@@ -1,3 +1,12 @@
 def add x, y
 	x + y
 end
+
+def titleize string
+  string.split(' ').map(&:capitalize).join(' ')
+end
+
+def rant s
+	s.upcase.gsub(" ", "") + "!!!"
+end
+ puts rant "i like pizza"
