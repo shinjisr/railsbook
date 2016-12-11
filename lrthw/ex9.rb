dcee5fd88a2cc01d2c1b83eb78a8ce3b4bd99a13
shinjisr <shinjisrg@gmail.com>
2016-12-11 18:00:35 +0800

    done exercise 8 and 9.

---
+++ lrthw/ex9.rb
@@ -0,0 +1,13 @@
+days = "Mon Tue Wed Thu Fri Sat Sun" #Fri>Fir
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts = "Here are the days: #{days}" #少打了 the
+puts = "Here are the months: #{months}" #漏掉 the
+
+#here in below seems have some syntax errors
+#puts %{
+#  There's something going on here.
+#  With the three double-quotes.
+#  We'll able to type as much as we like.
+#  Even 4 lines if we want, or 5, or 6.
+#}
