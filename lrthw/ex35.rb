6827bc243f2493b448ccb1128ad6fb5f9a6c8865
Nine_Bro <shinjisrg@gmail.com>
2017-04-24 21:37:02 +0800

    170424 give up to put boolean in if-statement

--- lrthw/ex35.rb
+++ lrthw/ex35.rb
@@ -31,15 +31,15 @@ def bear_room
 		print ">1.take honey > \n>2.taunt bear > \n>3.open door \n> "
 		choice = $stdin.gets.chomp
 
-		if choice == "take honey" or choice == "1"
+		if choice == "take honey" # or choice == "1"
 			dead("The bear looks at you then slaps your face off.")
-		elsif choice == ("taunt bear" or "2") && !bear_moved
+		elsif choice == "taunt bear" && !bear_moved
 		# elsif choice == "taunt bear" && !bear_moved
 			puts "The bear has moved from the door. You can go through it now."
-			bear_moved == true
-		elsif (choice == "taunt bear" or choice == "2") && bear_moved
+			bear_moved = true
+		elsif choice == "taunt bear" && bear_moved
 			dead("The bear gets pissed off and chews your leg off.")
-		elsif (choice == "open door" or choice == "3") && bear_moved
+		elsif choice == "open door" && bear_moved
 			gold_room
 		else
 			puts "I got no idea what that means."
